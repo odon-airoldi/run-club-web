0465cdd81b70d65de846b6b18ef9bcc8519af431
odon-airoldi <odon.airoldi@gmail.com>
2026-07-22 16:54:44 +0200

    feat: enhance workout creation form with additional fields and API integration

--- src/pages/WorkoutCreatePage.jsx
+++ src/pages/WorkoutCreatePage.jsx
@@ -10,27 +10,38 @@ export default function IndexPage() {
 
     const [name, setName] = useState(null);
 
-    // useEffect(() => {
-    //     fetch(`http://run-club-api.test/api/workouts/${id}`)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             setWorkout(data.results)
-    //         })
-
-    // }, [])
-
     function addNewWorkout(e) {
 
         e.preventDefault()
 
         const data = {
             name: e.target.name.value,
-            description: e.target.description.value
+            description: e.target.description.value,
+            date_time: `${e.target.date.value} ${e.target.time.value}:00`,
+            place_city: e.target.place_city.value,
+            place_address: e.target.place_address.value,
+            buffer_time: e.target.buffer_time.value,
+            distance: e.target.distance.value,
+            pace: Number(e.target.pace_m.value) * 60 + Number(e.target.pace_s.value)
         }
 
-        setNewWorkout(data)
+        // setNewWorkout(data)
 
-        console.log(newWorkout)
+        fetch('http://run-club-api.test/api/workouts/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+
+
+
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
 
     }
 
@@ -39,21 +50,47 @@ export default function IndexPage() {
             <h1>Aggiungi un allenamento</h1>
             <form onSubmit={addNewWorkout}>
                 <div>
-                    <label htmlFor="name">Nome</label>
+                    <label htmlFor="name">Name</label>
                     <input className="border" type="text" id="name" name="name" />
                 </div>
                 <div>
-                    <label htmlFor="description">Descrizione</label>
-                    <input className="border" type="text" id="description" name="description" />
+                    <label htmlFor="description">Description</label>
+                    <textarea className="border" type="text" id="description" name="description"></textarea>
+                </div>
+                <div>
+                    <label htmlFor="date">Data</label>
+                    <input className="border" type="date" id="date" name="date" />
+                </div>
+                <div>
+                    <label htmlFor="time">Ora</label>
+                    <input className="border" type="time" id="time" name="time" />
+                </div>
+                <div>
+                    <label htmlFor="place_city">Città</label>
+                    <input className="border" type="text" id="place_city" name="place_city" />
+                </div>
+                <div>
+                    <label htmlFor="place_address">Indirizzo</label>
+                    <input className="border" type="text" id="place_address" name="place_address" />
+                </div>
+                <div>
+                    <label htmlFor="buffer_time">Tempo di attesa</label>
+                    <input className="border" type="number" id="buffer_time" name="buffer_time" /> min
+                </div>
+                <div>
+                    <label htmlFor="distance">Distanza</label>
+                    <input className="border" type="number" id="distance" name="distance" /> Km
+                </div>
+                <div>
+                    <label htmlFor="pace_m">Passo</label>
+                    <input className="border" type="number" id="pace_m" name="pace_m" min="0" max="59" /> min
+                    <input className="border" type="number" id="pace_s" name="pace_s" min="0" max="59" /> sec
                 </div>
                 <div>
                     <button type="submit">Aggiungi</button>
                 </div>
             </form>
-
-            {name}
         </>
     );
-
 
 }
