9195ae22870fef700428d0b16853387c50d03de5
odon-airoldi <odon.airoldi@gmail.com>
2026-08-10 16:00:22 +0200

    feat: enhance workout display in IndexPage by adding units to distance and pace

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -30,8 +30,8 @@ export default function IndexPage() {
                                 <div>{new Date(workout.date_time).toLocaleDateString('it-IT')}</div>
                                 <h2 className="font-bold">{workout.name}</h2>
                                 <p>{workout.place_city}</p>
-                                <p>{workout.distance}</p>
-                                <p>{(workout.pace - workout.pace % 60) / 60}:{workout.pace % 60}</p>
+                                <p>{workout.distance} Km</p>
+                                <p>{(workout.pace - workout.pace % 60) / 60}:{(workout.pace % 60) === 0 ? '00' : workout.pace % 60} min/km</p>
                             </Link>
                         </div>
 
