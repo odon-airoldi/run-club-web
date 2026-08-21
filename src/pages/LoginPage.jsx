abe9fa1ac1b70abd32e0a2f61900753d43c4e4c6
odon-airoldi <odon.airoldi@gmail.com>
2026-08-21 20:09:03 +0200

    feat: update LoginPage and RegisterPage for improved accessibility and styling; fix autocomplete attributes and enhance form structure

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
                     <div>
                         <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">Email</label>
                         <div className="mt-2">
-                            <input id="email" type="email" name="email" required autocomplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            <input id="email" type="email" name="email" required autoCompletete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                     </div>
 
@@ -75,12 +75,12 @@ export default function LoginPage() {
                                 </div> */}
                         </div>
                         <div className="mt-2">
-                            <input id="password" type="password" name="password" required autocomplete="current-password" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            <input id="password" type="password" name="password" required autoCompletete="current-password" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                         </div>
                     </div>
 
                     <div>
-                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Accedi</button>
                     </div>
                 </form>
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -56,32 +56,60 @@ export default function RegisterPage() {
 
     return (
         <>
-            <h1>Register</h1>
 
-            <form onSubmit={handleRegister}>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input className="border" type="text" id="name" name="name" />
+
+
+
+
+
+            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Registrati</h2>
                 </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input className="border" type="email" id="email" name="email" />
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form onSubmit={handleRegister} className="space-y-6">
+                        <div>
+                            <label htmlFor="nome" className="block text-sm/6 font-medium text-gray-900">Nome</label>
+                            <div className="mt-2">
+                                <input id="name" type="text" name="name" required autoComplete="name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">Email</label>
+                            <div className="mt-2">
+                                <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">Password</label>
+                            </div>
+                            <div className="mt-2">
+                                <input id="password" type="password" name="password" required autoComplete="current-password" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password_confirmation" className="block text-sm/6 font-medium text-gray-900">Conferma Password</label>
+                            </div>
+                            <div className="mt-2">
+                                <input id="password_confirmation" type="password" name="password_confirmation" required autoComplete="current-password" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Registrati</button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm/6 text-gray-500">
+                        Sei già membro? <Link to="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">Accedi</Link>
+                    </p>
                 </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input className="border" type="password" id="password" name="password" />
-                </div>
-                <div>
-                    <label htmlFor="password">Conferma password</label>
-                    <input className="border" type="password" id="password_confirmation" name="password_confirmation" />
-                </div>
-                <div>
-                    <button type="submit">Registrati</button>
-                </div>
-                <div>
-                    <div>Sei già membro? <Link to="/login">Accedi</Link></div>
-                </div>
-            </form>
+            </div>
 
         </>
     );
