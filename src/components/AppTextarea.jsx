0f091094c13e03737f5baa55ab51b36d8de10072
odon-airoldi <odon.airoldi@gmail.com>
2026-09-04 14:48:06 +0200

    style: update input and textarea components to use consistent text size for improved readability

--- src/components/AppTextarea.jsx
+++ src/components/AppTextarea.jsx
@@ -7,7 +7,7 @@ export default function AppTextarea({ name, label, ...props }) {
             {label &&
                 <label htmlFor={name} className="block text-xs mb-1 uppercase text-mauve-400 mb-1">{label}</label>
             }
-            <textarea name={name} {...props} className="block w-full bg-white px-4 py-2 text-base text-mauve-600 outline-1 -outline-offset-1 outline-mauve-300 placeholder:uppercase placeholder:text-xs placeholder:text-mauve-400 focus:outline-indigo-400 sm:text-sm/6"></textarea>
+            <textarea name={name} {...props} className="block w-full bg-white px-4 py-2 text-sm text-mauve-600 outline-1 -outline-offset-1 outline-mauve-300 placeholder:uppercase placeholder:text-xs placeholder:text-mauve-400 focus:outline-indigo-400"></textarea>
         </div>
     )
 
