3606acb8e6034d5d5457da007037051b31f43279
odon-airoldi <odon.airoldi@gmail.com>
2026-09-04 15:39:12 +0200

    style: update modal handling and text for improved user interaction on WorkoutPage

--- src/pages/WorkoutPage.jsx
+++ src/pages/WorkoutPage.jsx
@@ -19,7 +19,7 @@ export default function WorkoutPage() {
     const { workout, showWorkout, getWorkoutPaceTime, getWorkoutDurationTime } = useWorkout();
     const [joinWorkout, setJoinWorkout] = useState(null);
     const now = new Date();
-    const [openModal, setOpenModal] = useState(false);
+    const [openModal, setOpenModal] = useState(0);
     const { minutes, seconds } = getWorkoutPaceTime(workout.pace)
 
 
@@ -84,8 +84,6 @@ export default function WorkoutPage() {
     }
 
 
-
-
     return (
         <div className="">
             <div className="border border-mauve-300 rounded-sm text-mauve-600">
@@ -126,7 +124,7 @@ export default function WorkoutPage() {
                             <p className="mb-6">{workout.description}</p>
 
                             <div className="flex justify-between items-end">
-                                <div>
+                                <div onClick={() => setOpenModal(2)} >
                                     <div className="flex gap-1 text-white text-sm/[30px] mb-2">
                                         <AppUserPicture user={workout.user} className="w-8 h-8" />
                                         {
@@ -144,6 +142,21 @@ export default function WorkoutPage() {
                                         }
                                     </div>
                                 </div>
+                                {openModal === 2 &&
+                                    <div onClick={() => setOpenModal(0)} className="fixed inset-0 bg-mauve-200/50 backdrop-blur-xs p-10">
+                                        <div className="sm:w-128 h-full sm:mx-auto bg-white text-center">
+                                            <h2>Partecipanti</h2>
+                                            <ul>
+                                                <li><Link to={`/user/${workout.user.id}`}>{workout.user.first_name} {workout.user.last_name}</Link></li>
+                                                {
+                                                    workout.users_run?.map((user) => (
+                                                        <li><Link to={`/user/${user.id}`}>{user.first_name} {user.last_name}</Link></li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                    </div >
+                                }
                                 <div>
                                     { // se la data del workout è maggiore rispetto alla data attuale
                                         new Date(workout.date_time) > now ?
@@ -152,7 +165,7 @@ export default function WorkoutPage() {
                                                 : // altrimenti se user non è proprietario del workout
                                                 userAuth?.id !== workout.user_id &&
                                                 <AppButton onClick={userJoinWorkout}>
-                                                    {joinWorkout ? 'Ti sei unito al workout' : 'Partecipa'}
+                                                    {joinWorkout ? 'Ti sei unito all\'allenamento' : 'Partecipa'}
                                                 </AppButton>
                                             )
                                             //altrimenti
@@ -203,9 +216,9 @@ export default function WorkoutPage() {
 
                     <Link to={`/workout/${id}/edit`} className="border border-mauve-300 text-xs px-4 py-2 uppercase text-mauve-400 text-center cursor-pointer">Modifica</Link>
 
-                    <button onClick={() => setOpenModal(true)} className="border border-mauve-300 text-xs px-4 py-2 uppercase text-mauve-400 text-center cursor-pointer">Elimina</button>
-                    {openModal &&
-                        <div onClick={() => setOpenModal(false)} className="fixed inset-0 bg-mauve-200/50 backdrop-blur-xs flex items-center justify-center">
+                    <button onClick={() => setOpenModal(1)} className="border border-mauve-300 text-xs px-4 py-2 uppercase text-mauve-400 text-center cursor-pointer">Elimina</button>
+                    {openModal === 1 &&
+                        <div onClick={() => setOpenModal(0)} className="fixed inset-0 bg-mauve-200/50 backdrop-blur-xs flex items-center justify-center">
                             <AppButton onClick={deleteWorkout}>Vuoi eliminare definitivamente l'allenamento?</AppButton>
                         </div >
                     }
