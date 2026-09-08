8588da8870abf66fadaac02f25ba87238733739b
odon-airoldi <odon.airoldi@gmail.com>
2026-09-08 14:25:29 +0200

    style: improve layout and user interaction for workout sorting in WorkoutsPage

--- src/components/AppInput.jsx
+++ src/components/AppInput.jsx
@@ -4,7 +4,7 @@ export default function AppInput({ className = '', name, label, ...props }) {
     return (
         <div>
             {label &&
-                <label htmlFor={name} className="block mb-1 uppercase text-xs text-mauve-400 mb-1">{label}</label>
+                <label htmlFor={name} className="block uppercase text-xs text-mauve-400 mb-1">{label}</label>
             }
             <input name={name} {...props} className={`block w-full bg-white px-4 py-2 text-sm text-mauve-600 outline-1 -outline-offset-1 outline-mauve-300 placeholder:uppercase placeholder:text-xs placeholder:text-mauve-400 focus:outline-indigo-400 ${className}`} />
         </div>
--- src/pages/WorkoutsPage.jsx
+++ src/pages/WorkoutsPage.jsx
@@ -51,24 +51,24 @@ export default function WorkoutsPage() {
         <div className="">
             <div className="text-center mb-8">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-indigo-600 font-semibold font-zalando">Allenamenti</h1>
+                <div className="flex justify-center gap-1 uppercase text-xs text-mauve-400 mt-4">
+                    <div>Ordina per</div>
+                    <select className="uppercase" value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+                        <option value="date_time&asc">Data: Crescente</option>
+                        <option value="date_time&desc">Data: Descrescente</option>
+                        <option value="distance&asc">Km: Crescente</option>
+                        <option value="distance&desc">Km: Descrescente</option>
+                        <option value="pace&asc">Min/Km: Crescente</option>
+                        <option value="pace&desc">Min/Km: Decrescente</option>
+                    </select>
+                </div>
             </div>
 
-            <div>
-                <div>Ordina per</div>
-                <select value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
-                    <option value="date_time&asc">Data: Crescente</option>
-                    <option value="date_time&desc">Data: Descrescente</option>
-                    <option value="distance&asc">Km: Crescente</option>
-                    <option value="distance&desc">Km: Descrescente</option>
-                    <option value="pace&asc">Min/Km: Crescente</option>
-                    <option value="pace&desc">Min/Km: Decrescente</option>
-                </select>
-            </div>
 
             <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
                 {
-                    workouts.map((workout) => (
-                        // sortedWorkouts(workouts).map((workout) => (
+                    // workouts.map((workout) => (
+                    sortedWorkouts(workouts).map((workout) => (
                         <AppWorkoutCard key={workout.id} workout={workout} />
                     ))
                 }
